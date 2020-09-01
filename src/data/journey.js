3642df4e8d880b0815b93b941bee44ab19593c0e
Bobby Neelon <robert.neelon@gmail.com>
2020-08-31 23:52:03 -0500

    journey3

--- src/data/journey.js
+++ src/data/journey.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FaAward, FaPaperPlane, FaBaseballBall } from 'react-icons/fa'
+import {FaBaseballBall} from 'react-icons/fa'
+import {GiOilPump, GiOilRig} from 'react-icons/gi'
 
 import Image1 from './../assets/images/image1.jpg'
 import Image2 from './../assets/images/image2.jpg'
@@ -9,28 +10,24 @@ const data = [
   {
     date: '2009-20014',
     headline: 'Teacher and Coach',
-    description: `I taught High School Math and coached Baseball, Football, and Basketball helping lead multiple teams to the regional tournament`,
+    description: `I taught High School Math and coached Baseball, Football, and Basketball, helping lead multiple teams to the state quarter finals.`,
     image: Image1,
     icon: <FaBaseballBall />,
   },
   {
-    date: 'November 26th 2011',
-    headline: 'Milestone 2',
-    description: `ulputate sapien nec sagittis aliquam
-    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
-    ligula ullamcorper.`,
+    date: '2014-2017',
+    headline: 'ConocoPhillips',
+    description: `As a Reservoir Engineering Technician, ConocoPhillips was my first introduction to Oil and Gas and Data Analytics.`,
     image: Image2,
-    icon: <FaPaperPlane />,
+    icon: <GiOilPump />,
   },
 
   {
-    date: 'June 6th 2011',
-    headline: 'Milestone 3',
-    description: `ulputate sapien nec sagittis aliquam
-    malesuada. Quis imperdiet massa tincidunt nunc pulvinar sapien et
-    ligula ullamcorper.`,
+    date: '2017-2019',
+    headline: 'Reservoir Data Systems',
+    description: `As Software Architect and member of the Executive Team, I learned a ton about business, completions engineering, and software development.`,
     image: Image3,
-    icon: <FaPaperPlane />,
+    icon: <GiOilRig />,
   },
 ]
 
